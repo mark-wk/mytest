137e388ed8bc27051d8ad0acff62f797077f3128
wangkui <mark.wangkui@qq.com>
2022-04-25 11:08:03 +0800

    首次提交

---
+++ src/test/java/com/wangkui/mytest/Test.java
@@ -0,0 +1,16 @@
+package com.wangkui.mytest;
+
+import com.alibaba.fastjson.JSON;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author wangkui
+ * @date 2022/04/04
+ */
+public class Test {
+    public static void main(String[] args) {
+        byte[] bytes = "1".getBytes(StandardCharsets.UTF_8);
+        System.out.println("1".getBytes(StandardCharsets.UTF_8));
+    }
+}
